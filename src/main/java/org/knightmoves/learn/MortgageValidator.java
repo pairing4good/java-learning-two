9cea15737c52a0d18c064024f585d4b966d6ced6
dkessler <pairing4good@gmail.com>
2022-04-07 08:20:01 -0500

    The validation that should have been there initially

--- src/main/java/org/knightmoves/learn/MortgageValidator.java
+++
@@ -1,7 +0,0 @@
-package org.knightmoves.learn;
-
-public class MortgageValidator {
-    public boolean isValid(int principal, float annualInterest, byte years) {
-        return principal >= 0 && annualInterest >= 0 && years >= 0;
-    }
-}
